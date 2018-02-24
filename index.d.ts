24f8766d673710e7cf2ab45119758b1fceecc24f
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-02-24 16:04:17 +0000

    Moved index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import ConfigService from './src/ConfigModule/config.service';
+import ConfigModule from './src/ConfigModule/main';
+
+export default ConfigModule;
+export {ConfigService};
--- src/ConfigModule/index.d.ts
+++
@@ -1,5 +0,0 @@
-import ConfigService from './config.service';
-import ConfigModule from './main';
-
-export default ConfigModule;
-export {ConfigService};
