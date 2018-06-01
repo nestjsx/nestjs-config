1782dfd6747c8d7de5b030bee8f4b5d141d16664
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-06-01 22:24:11 +0100

    Fucking vscode can't save a file properly...

--- src/ConfigModule/config.service.ts
+++ src/ConfigModule/config.service.ts
@@ -1,40 +1,34 @@
-import { Injectable, LoggerService } from "@nestjs/common";
-import * as dotenv from "dotenv";
+import {
+    Injectable,
+} from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 @Injectable()
 export default class ConfigService {
-  private readonly config: object;
+    private readonly config: object;
+    constructor() {
+        const config = dotenv.config();
+        if (config.hasOwnProperty('parsed')) this.config = config.parsed;
+    }
 
-  constructor(private readonly logger: LoggerService) {
-    const config = dotenv.config();
-    logger.log(
-      `Loading config from: ${
-        typeof process !== "undefined"
-          ? process.cwd()
-          : "process is not defined"
-      }`
-    );
-    if (config.hasOwnProperty("parsed")) this.config = config.parsed;
-  }
+    /**
+     * Get the param or use default
+     *
+     * @param param
+     * @param def default
+     * @returns {any}
+     */
+    get(param : string, def : any = null) : any {
+        return this.has(param) ? this.config[param] : def;
+    }
 
-  /**
-   * Get the param or use default
-   *
-   * @param param
-   * @param def default
-   * @returns {any}
-   */
-  get(param: string, def: any = null): any {
-    return this.has(param) ? this.config[param] : def;
-  }
-
-  /**
-   * Check the param exists
-   *
-   * @param param
-   * @returns {boolean}
-   */
-  has(param: string): boolean {
-    return this.config.hasOwnProperty(param);
-  }
+    /**
+     * Check the param exists
+     *
+     * @param param
+     * @returns {boolean}
+     */
+    has(param : string) : boolean {
+        return this.config.hasOwnProperty(param);
+    }
 }
