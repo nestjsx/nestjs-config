e840f0ad797f37513ce4154865f683b000417ed9
ashleigh <ashleigh@wi-q.com>
2018-07-06 12:19:49 +0100

    Updated example
    Signed-off-by: ashleigh <ashleigh@wi-q.com>

--- examples/UserModule/user.controller.ts
+++ examples/UserModule/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from "@nest/common";
-
 import { ConfigService } from "@bashleigh/nest-config";
+import * as pug from 'pug';
 
 @Controller("user")
 export default class UserController {
@@ -15,5 +15,13 @@ export default class UserController {
       username: username,
       live: live
     };
+  }
+
+  @Get('test')
+  test() {
+    //just an example for src
+    return pug.renderFile(this.config.src('templates/test.pug', {
+      title: this.config.get('TITLE'),
+    }));
   }
 }
