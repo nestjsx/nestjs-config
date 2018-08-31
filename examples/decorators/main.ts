31a47adac02f6555dbf7a4f8a3453d6dd3ba6e24
Shady Khalifa <shekohex@gmail.com>
2018-08-31 05:15:07 +0200

    formating files

--- examples/decorators/main.ts
+++ examples/decorators/main.ts
@@ -1,10 +1,12 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule} from 'nestjs-config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
 import UserController from './user.controller';
 import * as path from 'path';
 
 @Module({
-    imports: [ConfigModule.load(path.resolve(__dirname, 'config', '*/**.{ts,js}'))],
-    controllers: [UserController],
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '*/**.{ts,js}')),
+  ],
+  controllers: [UserController],
 })
 export default class UserModule {}
