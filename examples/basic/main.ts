4138f57353ed075ec8626e07351f6796c67e7ada
Shady Khalifa <shekohex@gmail.com>
2018-08-31 05:15:07 +0200

    formating files

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from 'nestjs-config';
+import { ConfigModule } from 'nestjs-config';
 import * as path from 'path';
 
 import UserController from './user.controller';
 
 @Module({
-  imports: [ConfigModule.load(path.resolve(__dirname, 'config', '*/**.{ts,js}'))],
+  imports: [
+    ConfigModule.load(path.resolve(__dirname, 'config', '*/**.{ts,js}')),
+  ],
   controllers: [UserController],
 })
 export default class UserModule {}
--- examples/typeorm/index.ts
+++ examples/typeorm/index.ts
@@ -1,19 +1,19 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule, ConfigService} from 'nestjs-config';
-import {TypeOrmModule} from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 
 //Load the configservice before the initial module load
 ConfigService.load(path.resolve(__dirname, 'config/**/*.{ts,js}'));
 
 @Module({
-    imports: [
-        ConfigModule.load(),
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            useFactory: async (config: ConfigService) => config.get('database'),
-            inject: [ConfigService],
-        }),
-    ],
+  imports: [
+    ConfigModule.load(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => config.get('database'),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export default class AppModule {}
