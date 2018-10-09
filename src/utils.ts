df55e515f8d53c63defa8e6541702c76cd90a696
Andrew Yustyk <a.yustyk@gmail.com>
2018-10-10 00:15:35 +0300

    More strict checks in decorators

--- src/utils.ts
+++ src/utils.ts
@@ -9,15 +9,22 @@ export function applyParamsMetadataDecorator(
     // Override the original parameter value
     // with the expected property of the value even a deep property.
     for (const param of paramsMetadata) {
-      if (param.configKey) {
+      if (Object.keys(param).includes('configKey')) {
         const i = param.parameterIndex;
         if (args[i] instanceof ConfigService || args[i] === ConfigService) {
           // if parameter is a ConfigService instance or ConfigService cass itself
           // then retrieve required config param from this instance
           args[param.parameterIndex] = args[i].get(param.configKey, param.fallback);
-        } else if (args[i] === undefined || args[i] === null) {
-          // if parameter is null or undefined
-          // then retrieve required config param from passed fn
+        } else if (args[i] === undefined) {
+          // populate undefined argument with the config parameter value
+          args[param.parameterIndex] = fn(param.configKey, param.fallback);
+        } else if (args[i] === null) {
+          /**
+           * nestjs router populates missing parameters with nulls by default
+           * so we need to populate nullable argument with the config parameter value too.
+           * @link https://github.com/nestjs/nest/blob/master/packages/core/router/router-execution-context.ts#L117
+           * @link https://github.com/nestjs/nest/issues/1182
+           */
           args[param.parameterIndex] = fn(param.configKey, param.fallback);
         }
       }
