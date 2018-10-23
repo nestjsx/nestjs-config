fb181d8420b4828a4236b50229c7efe950e6269b
Andrew Yustyk <a.yustyk@gmail.com>
2018-10-24 02:20:39 +0300

    Improve Configurable decorator due to resolving nestjs issue #1182

--- src/utils.ts
+++ src/utils.ts
@@ -18,14 +18,6 @@ export function applyParamsMetadataDecorator(
         } else if (args[i] === undefined) {
           // populate undefined argument with the config parameter value
           args[param.parameterIndex] = fn(param.configKey, param.fallback);
-        } else if (args[i] === null) {
-          /**
-           * nestjs router populates missing parameters with nulls by default
-           * so we need to populate nullable argument with the config parameter value too.
-           * @link https://github.com/nestjs/nest/blob/master/packages/core/router/router-execution-context.ts#L117
-           * @link https://github.com/nestjs/nest/issues/1182
-           */
-          args[param.parameterIndex] = fn(param.configKey, param.fallback);
         }
       }
     }
