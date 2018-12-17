cc7305d8e365101e3e9771b8cccd7263ab466e63
Ashleigh Simonelli <ashleigh@wi-q.com>
2018-12-17 11:24:44 +0000

    Update index.ts

--- examples/typeorm/index.ts
+++ examples/typeorm/index.ts
@@ -3,14 +3,10 @@ import { ConfigModule, ConfigService } from 'nestjs-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 
-//Load the configservice before the initial module load
-ConfigService.load(path.resolve(__dirname, 'config/**/*.{ts,js}'));
-
 @Module({
   imports: [
-    ConfigModule.load(),
+    ConfigModule.load(path.resolve(__dirname, 'config/**/*.{ts,js}')),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: async (config: ConfigService) => config.get('database'),
       inject: [ConfigService],
     }),
