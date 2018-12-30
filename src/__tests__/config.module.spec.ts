0ce5ce313c9cee55bbf3f507c8c49cff9cbf0538
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-12-30 15:49:51 +0000

    Extended from Dotenv options

--- src/__tests__/config.module.spec.ts
+++ src/__tests__/config.module.spec.ts
@@ -222,8 +222,7 @@ describe('Config Nest Module', () => {
 
       @Configurable()
       testConfig(
-        @ConfigParam('config.doesntexists', 'test123')
-        configKey?: string,
+        @ConfigParam('config.doesntexists', 'test123') configKey?: string,
       ) {
         return configKey;
       }
@@ -305,19 +304,19 @@ describe('Config Nest Module', () => {
   });
 
   it('Will allow to override config name key', async () => {
-      const module = await Test.createTestingModule({
-          imports: [
-              ConfigModule.load(path.resolve(__dirname, '__stubs__', '**/*.ts'), {
-                modifyConfigName: (name) => name.replace('config.', ''),
-              }),
-          ],
-      }).compile();
+    const module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.load(path.resolve(__dirname, '__stubs__', '**/*.ts'), {
+          modifyConfigName: name => name.replace('config.', ''),
+        }),
+      ],
+    }).compile();
 
-      const configService = module.get<ConfigService>(ConfigService);
+    const configService = module.get<ConfigService>(ConfigService);
 
-      expect(configService.get('config.server')).toBeUndefined();
-      expect(configService.get('config.stub')).toBeUndefined();
-      expect(configService.get('server')).toBeTruthy();
-      expect(configService.get('stub')).toBeTruthy();
+    expect(configService.get('config.server')).toBeUndefined();
+    expect(configService.get('config.stub')).toBeUndefined();
+    expect(configService.get('server')).toBeTruthy();
+    expect(configService.get('stub')).toBeTruthy();
   });
 });
