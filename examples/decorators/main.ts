a1bca181ffae08b4fd5b890e4c6a857c7cae8f8b
ashleigh <ashleigh@wi-q.com>
2019-01-23 10:30:03 +0000

    Updated examples and cleaned up files

--- examples/decorators/main.ts
+++ examples/decorators/main.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 
 @Module({
   imports: [
-    ConfigModule.load(path.resolve(__dirname, 'config', '*/**.{ts,js}')),
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/(!*.d).{ts,js}')),
   ],
   controllers: [UserController],
 })
--- src/__tests__/__stubs__/config.env.ts
+++ src/__tests__/__stubs__/config.env.ts
@@ -1,5 +1,3 @@
-
-
 export default {
   project: process.env.PROJECT_NAME
 };
