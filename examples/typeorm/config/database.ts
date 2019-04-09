cec6502283031576b432b87dbe5602590986fc7c
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2019-04-09 14:45:10 +0100

    Updated database class example

--- examples/typeorm/config/database.ts
+++ examples/typeorm/config/database.ts
@@ -1,11 +1,13 @@
-export default {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    logging: process.env.DB_LOGGING == 'true',
-    sync: process.env.DB_SYNC == 'true',
-    entities: ['src/entities/*.entity.ts'],
-    port: parseInt(process.env.DB_PORT),
+import {TypeOrmOptions} from 'typeorm';
+
+export default class DatabaseConfig implements TypeOrmOptions {
+    public type: 'mysql' | 'monogodb' = 'mysql';
+    public host: string = process.env.TYPEORM_HOST;
+    public username: string = process.env.TYPEORM_USERNAME;
+    public password: string = process.env.TYPEORM_PASSWORD;
+    public database: string = process.env.TYPEORM_DATABASE;
+    public logging: boolean = process.env.TYPEORM_LOGGING === 'true';
+    public sync: boolean = process.env.TYPEORM_SYNCHRONIZE === 'true';
+    public entities: string[] = process.env.TYPEORM_ENTITIES.split(',');
+    public port: number = parseInt(process.env.TYPEORM_PORT);
 };
