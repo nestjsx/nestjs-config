c27f9346c2c26d95d7ae1d12582e07b3da2145ea
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2019-05-13 18:02:24 +0100

    Resolved test issues

--- src/__tests__/__stubs__/config/config.ts
+++ src/__tests__/__stubs__/config/config.ts
@@ -1,3 +1,4 @@
 export default {
+  port: 2000,
   project: process.env.PROJECT_NAME,
 };
---
+++ src/__tests__/__stubs__/config/named.ts
@@ -0,0 +1,5 @@
+export default {
+  __name: 'tester_test',
+  __provide: 'testy_test',
+  test: 'hello',
+};
